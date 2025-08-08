bdb91b6367f003caa9ba28196d1de66f983180a5
yogiswara.utama <yogiswara.utama@tuntun.co.id>
2025-08-08 12:10:15 +0700

    feat: initialize project with Vite, React, and Material UI
    - Added package.json with dependencies for React, Material UI, and React Query.
    - Created Vite configuration file for project setup.
    - Added TypeScript configuration files for app and node environments.
    - Implemented main application structure with App component using Material UI Button.
    - Included global CSS styles and reset with CssBaseline from Material UI.
    - Added SVG assets for Vite and React logos.
    - Created App.css for styling the application layout and components.
    - Set up index.css for global styles and theming based on user preferences.
    - Integrated React Query for data fetching and state management.

---
+++ src/main.tsx
@@ -0,0 +1,20 @@
+import CssBaseline from '@mui/material/CssBaseline';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <CssBaseline />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  </StrictMode>
+);
